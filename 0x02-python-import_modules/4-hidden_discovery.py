f28afc65eb5c444febec3818d3f4d73eeb6116c7
AgaiMorara <corneliusmorara@gmail.com>
2023-08-16 20:15:16 +0300

    delta changes

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@
 import dis
 import importlib.util
 
+
 def print_module_names(module_path):
     spec = importlib.util.spec_from_file_location("hidden_4", module_path)
     module = importlib.util.module_from_spec(spec)
@@ -13,6 +14,7 @@ def print_module_names(module_path):
 
     for name in names:
         print(name)
+
 
 if __name__ == "__main__":
     print_module_names("hidden_4.pyc")
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 
+
 def prita():
     print(a)
+
 
 if __name__ == "__main__":
     prita()
