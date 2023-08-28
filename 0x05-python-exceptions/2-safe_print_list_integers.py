96985084965ba2a97f9499616fce41ef295c2e7f
AgaiMorara <corneliusmorara@gmail.com>
2023-08-28 20:52:34 +0300

    nested tries

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    try:
+        index = 0
+        while index < x:
+            try:
+                print("{:d}".format(my_list[index]), end='')
+                printed += 1
+                index += 1
+            except(ValueError,TypeError):
+                index += 1
+    except (IndexError, ValueError, TypeError):
+        pass
+    print()
+    return printed
