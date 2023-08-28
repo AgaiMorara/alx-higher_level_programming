dbf867f279df3e7e4bc502eada047cd419067cb2
AgaiMorara <corneliusmorara@gmail.com>
2023-08-28 07:39:03 +0300

    negavite powers

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if b == 0:
+        return 1
+
     result = 1
     if b > 0:
-        for ans in range(b):
+        for _ in range(b):
             result *= a
     else:
-        for ans in range(-b):
+        for _ in range(-b):
             result /= a
     return result
