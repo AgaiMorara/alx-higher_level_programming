8c03ce8b0c4bf3040c9577888566917b5aa510a5
AgaiMorara <corneliusmorara@gmail.com>
2023-08-29 17:07:07 +0300

    documented square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" Let us make all squares to have a field called size"""
+
+
 class Square:
     """ defines a Square with private instance attribute size """
     def __init__(self, size=0):
+        """ Instantiates the class square
+            Args:
+            @size: side of the square """
         self.__size = size
