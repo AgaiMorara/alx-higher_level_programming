802f631be65e0dade2412166e5e497738cf81283
AgaiMorara <corneliusmorara@gmail.com>
2023-08-29 16:50:58 +0300

    private att

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 class Square:
-    """
-    defines a square with private instance attribute size
-    """
-
+    """ defines a Square with private instance attribute size """
     def __init__(self, size=0):
         self.__size = size
