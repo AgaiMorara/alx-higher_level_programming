d8c877ec1247c28d1731ea03167ea3ebf0a342ed
AgaiMorara <corneliusmorara@gmail.com>
2023-09-06 00:16:20 +0300

    try ints

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
     Adds two integers and returns the result.
 
     Parameters:
-    a (int): The first integer.
+    a (int/float): The first integer.
     b (int, optional): The second integer. Defaults to 98.
 
     Returns:
@@ -24,12 +24,14 @@ def add_integer(a, b=98):
     Raises:
     TypeError: If a or b is not an integer.
     '''
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError("Both 'a' and 'b' must be integers")
+    if not isinstance(a, (int,float)):
+        raise TypeError("a must be an integer")
 
-    return a + b
+    if not isinstance(b, (int, float)):
+        raise TypeError("b mus be an integer")
 
-# Test cases
+    return  int (a + b)
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
