61f3b6e4213e4b83aee50cd5f7b61618c38a017e
AgaiMorara <corneliusmorara@gmail.com>
2023-09-06 00:47:27 +0300

    could not get it over line

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Further practice... The function Below will divide a matrix
+matrix_divided - function that performs the operation
+"""
+
+def matrix_divided (matrix, div):
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
