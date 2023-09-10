709e80e31b0daacbffe7d94300990323c8a1f18f
AgaiMorara <corneliusmorara@gmail.com>
2023-09-10 11:23:39 +0300

    __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            # Allow setting the 'first_name' attribute
-            self.__dict__[name] = value
-        else:
-            raise AttributeError("object has no attribute.")
+    def __slots__ = ['first_name']
