7a026fe03622d6563c271fa8369b56727c79c870
AgaiMorara <corneliusmorara@gmail.com>
2023-09-10 12:14:30 +0300

    documented

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,12 +3,12 @@
 A restricted class for memory optimization.
 """
 
+
 class LockedClass:
     """
-    This class defines a restricted class using __slots__ for memory optimization.
+    This class defines a restricted class using __slots__ for memory optimiz
 
     Attributes:
         first_name (str): A string representing the first name of an object.
     """
     __slots__ = ['first_name']
-
