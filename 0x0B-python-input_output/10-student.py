6fda3cec52bbc7f425dc36729f9a626e19ab62ef
AgaiMorara <corneliusmorara@gmail.com>
2023-09-14 01:18:41 +0300

    cleaner

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-writes a class that defines a student
+Writes a class that defines a student
 """
 
 
@@ -12,9 +12,9 @@ class Student:
         """
         Initialize the student
         Args:
-        first_name(str): self_explanatory
-        last_name(str) : self explanatory
-        age (int) : today - dob
+        first_name (str): self_explanatory
+        last_name (str): self explanatory
+        age (int): today - dob
         """
 
         self.first_name = first_name
@@ -23,15 +23,14 @@ class Student:
 
     def to_json(self, attrs=None):
         """
-        Retrieves a dictionary representation of a student instance.
-        and returns
-        it
+        Retrieves a dictionary representation of a student instance
+        and returns it.
         """
         if attrs is None:
             return self.__dict__
+        elif isinstance(attrs, list):
+            are_all_strings = all(isinstance(attr, str) for attr in attrs)
+            if are_all_strings:
+                return {h: getattr(self, h) for h in attrs if hasattr(self, h)}
         else:
-            result = {}
-            for attr in attrs:
-                if hasattr(self, attr):
-                    result[attr] = getattr(self, attr)
-                return result
+            return self.__dict__
