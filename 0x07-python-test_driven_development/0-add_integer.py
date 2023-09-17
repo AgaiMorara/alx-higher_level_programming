56344a17617204e871b1a5f295abecdbb871a772
AgaiMorara <corneliusmorara@gmail.com>
2023-09-17 18:37:40 +0300

    spellings correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,6 +10,7 @@ TypeError: Both 'a' and 'b' must be integers
 100
 """
 
+
 def add_integer(a, b=98):
     '''
     Adds two integers and returns the result.
@@ -24,13 +25,14 @@ def add_integer(a, b=98):
     Raises:
     TypeError: If a or b is not an integer.
     '''
-    if not isinstance(a, (int,float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    return  int (a + b)
+    return int(a + b)
+
 
 if __name__ == "__main__":
     import doctest
