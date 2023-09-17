9dec72240119dbc7d6dbd218c10d7c41cb000297
AgaiMorara <corneliusmorara@gmail.com>
2023-09-17 18:33:50 +0300

    spellings correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -28,7 +28,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
-        raise TypeError("b mus be an integer")
+        raise TypeError("b must be an integer")
 
     return  int (a + b)
 
