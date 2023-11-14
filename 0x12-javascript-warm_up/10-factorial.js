31edbea9f0cd53621e1843dec35d37fdec04ea8c
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 18:53:02 -0800

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  // Recursive case: n! = n * (n-1)!
+  return n * factorial(n - 1);
+}
+
+const input = parseInt(process.argv[2]);
+
+if (isNaN(input)) {
+  console.log('Factorial of NaN is 1');
+} else {
+  const result = factorial(input);
+  console.log(result);
+}
