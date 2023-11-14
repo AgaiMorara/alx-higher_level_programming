aa8e96180d324d8b639d339b9f19513ce2bfaff3
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 17:15:30 -0800

    concat like py

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const first = process.argv[2];
-const second = process.arv[3];
+const second = process.argv[3];
 
-console.log(first + 'is' + second);
+console.log(first + ' is ' + second);
