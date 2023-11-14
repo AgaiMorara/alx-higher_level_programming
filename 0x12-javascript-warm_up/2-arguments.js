b3ac4bdd704c3b08f502d4ddf05aa304c358c2fb
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 16:54:22 -0800

    process.argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const totalags = process.argv.length - 2;
+
+if (totalags === 0) {
+  console.log('No argument');
+} else if (totalags === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
