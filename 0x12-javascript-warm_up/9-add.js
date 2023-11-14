30a4e202f35e6054f63b31ed0309526d5d87c127
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 18:43:09 -0800

    added

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const firstarg = parseInt(process.argv[2]);
+const secondarg = parseInt(process.argv[3]);
+
+if (isNaN(firstarg) || isNaN(secondarg)) {
+  console.log('Missing or invalid input. Please provide two integers.');
+} else {
+  const result = add(firstarg, secondarg);
+  console.log(firstarg + ' + ' + secondarg + ' = ' + result);
+}
