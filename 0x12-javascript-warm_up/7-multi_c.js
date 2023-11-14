603273bec015e18fb36263e448308a4a9ba939b4
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 18:19:20 -0800

    loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,4 +2,4 @@
 let x = process.argv[2];
 if (isNaN(parseInt(x))) { console.log('Missing number of occurrences'); }
 
-while (x--) { console.log('C is fun'); }
+while (x-- && x > 0) { console.log('C is fun'); }
