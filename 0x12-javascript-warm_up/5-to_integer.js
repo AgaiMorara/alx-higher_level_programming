6d5b351617e257fbf542f95e621802792d9b6ea1
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 17:37:03 -0800

    to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+let argument = process.arg[2];
+
+const castarg = parseInt(dec, argument);
+
+if (castarg !== NaN)) {
+  console.log('My number :' + castarg);
+} else { console.log('Not a number'); }
