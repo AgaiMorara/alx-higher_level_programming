55c9ea4cf641de1561aced3d82225382f9b86316
AgaiMorara <corneliusmorara@gmail.com>
2023-11-13 18:55:30 -0800

    bubble lookup

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  // Remove duplicates and sort in descending order
+  const uniqueSortedArgs = [...new Set(args)].sort((a, b) => b - a);
+
+  // The second element is the second biggest integer
+  const secondBiggest = uniqueSortedArgs[1];
+  console.log(secondBiggest || 0);
+}
