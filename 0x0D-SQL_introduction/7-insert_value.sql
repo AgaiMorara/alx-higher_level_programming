2f142840f7b4cd1a57c2eb29869e64f568f9ee28
AgaiMorara <corneliusmorara@gmail.com>
2023-11-23 01:22:39 -0800

    insert value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- inserts a new row in the table first_table in the server
 -- Remeber: in all commands we write... we presume connection to hbtn_0c_0
-INSERT INTO first_table (id, name) VALUES (89, 'Best school');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
