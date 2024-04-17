12c25b92a3bbad354cf1282d72f716adb638a9b9
AgaiMorara <corneliusmorara@gmail.com>
2024-04-18 01:45:26 +0300

    substr

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a url and displays all HTTP methods the server will accept
-curl -s -X OPTIONS -i $1 | awk '/Allow:/ {print $2}'
+curl -sI -X OPTIONS $1 | grep -i 'Allow:' | awk '{print substr($0, index($0, ":") + 2)}' 
