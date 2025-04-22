31555ded20c9be999862bd4ae00e426ba73f4a26
AgaiMorara <corneliusmorara@gmail.com>
2025-04-22 12:03:34 +0300

    git check

---
+++ 0x14-javascript-web_scraping/filehandling.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// C File handling
+//1. Stream-oriented data files - the data is stored in the same manner as it appears on your screen
+//2. System-oriented data files - the data files are more closely associated with the OS 
+// Major operations: {creation of a new file, opening, reading, writing data in a file, closing a file.}
+
+// Steps for Processing a file
+// 1. Declare a file pointer variable
+// 2. Open a file using fopen()
+// 3. Process the file using the suitable function
+// 4. close the file using fclose() function
+// 5. 
+
+int main()
+{
+
+}
