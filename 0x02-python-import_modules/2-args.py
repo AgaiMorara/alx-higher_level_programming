8f87e1660667061e89e5d731b5190d835829bc31
AgaiMorara <corneliusmorara@gmail.com>
2025-05-07 10:02:42 +0300

    only script run

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+"""This script prints the number and list of arguments passed via the command line."""
+
 import sys
-"""                                                                                                                                                                                                         
- This script finds the lenght of argv, and returns the output                                                                                                                                               
-"""
-number = len(sys.argv) - 1
-if number == 1:
-    print(f"{number} argument:\n1: {sys.argv[1]}")
-elif number == 0:
-    print(f"{number} arguments.")
-else:
-    print(f"{number} arguments:")
-    for index, string in enumerate(sys.argv[1:]):
-        print(f"{index + 1}: {string}")
 
-
+if __name__ == "__main__":
+    number = len(sys.argv) - 1
+    if number == 0:
+        print(f"{number} arguments.")
+    elif number == 1:
+        print(f"{number} argument:\n1: {sys.argv[1]}")
+    else:
+        print(f"{number} arguments:")
+        for index, string in enumerate(sys.argv[1:]):
+            print(f"{index + 1}: {string}")
