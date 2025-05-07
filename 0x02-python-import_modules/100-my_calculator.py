0b9460040b31c3dcffc34f742ac0e48a791ae5ee
AgaiMorara <corneliusmorara@gmail.com>
2025-05-07 15:00:12 +0300

    syntax changes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     if (len(sys.argv) - 1 != 3):
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if (sys.argv[2] not in '+-*/'):
         print("Unknown operator. Available operators: +, -, * and /")
